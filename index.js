520e8010ec4eacfe1114ea222ddf52bd9964e2a2
mm Kraichan <swordmannong_surin@hotmail.com>
2017-09-09 14:39:53 +0700

    how to initiate variable

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world for nong');
+var y = '3fdsa';
+console.log('y', y);
+function nongHelloworld () {
+    const x = 3;
+    let z = 'hello ';
+    z = 4;
+    var y = "test";
+    console.log('y', y);
+    console.log('x', typeof x);
+    console.log('z', typeof z);
+    console.log('z', z);
+}
+
+nongHelloworld();
